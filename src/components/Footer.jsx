e7229be233756df12eef300f0be51c3f72f6c254
Sagar Sharma <sagar9012sharma@gmail.com>
2026-05-04 18:54:47 +0530

    Add files via upload

---
+++ src/components/Footer.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <>
+      <div className="item-center justify-center bg-gray-800 text-white">
+        <hr className=" border-black" />
+        <h1 className="text-center py-3 text-sm">
+          Copyright © 2026 - All right reserved by Sagar Sharma
+        </h1>
+      </div>
+    </>
+  );
+}
+
+export default Footer;
